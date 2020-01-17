fe21ebc062072cbdacbe8acb9819fd194d23539d
vicencio-tech <bvicencio.l@gmail.com>
2020-01-17 10:46:23 -0300

    agregando funcion de ingreso y registro de usuario

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-// Este es el punto de entrada de tu aplicacion
+import {register} from './lib/index.js';
+import {passIn} from './lib/index.js';
+import {observer} from './lib/index.js';
+import {signOff} from './lib/index.js';
+import {showUp} from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+document.getElementById('send').addEventListener('click',function(){
+    let email=document.getElementById('email').value;
+    let password=document.getElementById('password').value;
+    register(email, password);
+});
 
-myFunction();
+document.getElementById('access').addEventListener('click',function(){
+    let email2=document.getElementById('email2').value;
+    let password2=document.getElementById('password2').value;
+    passIn(email2, password2);
+});
+
+observer();
+
+showUp();
+
+document.getElementById('logout').addEventListener('click',function(){
+    signOff();
+});
